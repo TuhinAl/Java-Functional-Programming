6ac20fb89748900a72702958baad82280d2a5956
alauddintuhin1411@gmail.com <alauddintuhin1411@gmail.com>
2022-06-05 17:10:19 +0600

    filter using predicate

--- src/streamApi/chapterTwo/FilteringApples.java
+++ src/streamApi/chapterTwo/FilteringApples.java
@@ -1,13 +1,20 @@
 package streamApi.chapterTwo;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.function.Predicate;
 
 public class FilteringApples {
 
 	public static void main(String[] args) {
-		
+		  List<Apple> inventory = Arrays.asList(new Apple(80,"green"),
+                  new Apple(155, "green"),
+                  new Apple(120, "red"));	
+		  
+		  
+		  List<Apple> apples = filterApples(inventory, FilteringApples::isGreenApple);
+		  System.out.println("apples: "+apples);
 
 	}
 	
