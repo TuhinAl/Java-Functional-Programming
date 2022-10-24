352d5bc8e3c659bb94f284d0cede94ed6f888a9f
tuhinal <alauddintuhin1411@gmail.com>
2022-10-24 18:26:26 +0600

    groupBy and count of a List of object

--- src/stream/ParallelStream.java
+++ src/stream/ParallelStream.java
@@ -1,6 +1,9 @@
 package stream;
 
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import functionalInterface.CustomData;
 import functionalInterface.Student;
@@ -9,11 +12,22 @@ public class ParallelStream {
 
 	public static void main(String[] args) {
 		List<Student> studentList = CustomData.getData();
-//		int sum = studentList
+//		 studentList
 //				.parallelStream()
 //				.filter(e -> e.getAge() < 30 && e.getAge() > 20)
-//				.reduce(0, Integer::sum);
-
+//				.map(cgpa -> cgpa.getCgpa()+2)
+//				.forEach(e -> System.out.println(e));
+		 
+		 // groupBy a list and count
+		 Map<String, Long> studentGroupByAndCount = studentList
+				 .stream()
+				 .collect(
+						 Collectors.groupingBy(
+								 e -> e.getGender(), Collectors.counting()
+								 )
+						 );
+		 
+System.out.println(studentGroupByAndCount);
 	}
 
 }
