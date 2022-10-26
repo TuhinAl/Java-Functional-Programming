392d283fd013587ea80a27e0db5295545f789f4f
tuhinal <alauddintuhin1411@gmail.com>
2022-10-26 12:06:03 +0600

    sorted, groupby, count of a stream of list

--- src/stream/ParallelStream.java
+++ src/stream/ParallelStream.java
@@ -1,5 +1,7 @@
 package stream;
 
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
@@ -12,22 +14,64 @@ public class ParallelStream {
 
 	public static void main(String[] args) {
 		List<Student> studentList = CustomData.getData();
-//		 studentList
-//				.parallelStream()
-//				.filter(e -> e.getAge() < 30 && e.getAge() > 20)
-//				.map(cgpa -> cgpa.getCgpa()+2)
-//				.forEach(e -> System.out.println(e));
-		 
-		 // groupBy a list and count
-		 Map<String, Long> studentGroupByAndCount = studentList
-				 .stream()
-				 .collect(
-						 Collectors.groupingBy(
-								 e -> e.getGender(), Collectors.counting()
-								 )
-						 );
-		 
-System.out.println(studentGroupByAndCount);
+		//		 studentList
+		//				.parallelStream()
+		//				.filter(e -> e.getAge() < 30 && e.getAge() > 20)
+		//				.map(cgpa -> cgpa.getCgpa()+2)
+		//				.forEach(e -> System.out.println(e));
+
+		// groupBy gender a list and count
+		Map<String, Long> studentGroupByAndCount = studentList
+				.stream()
+				.collect(
+						Collectors.groupingBy(
+								e -> e.getGender(), Collectors.counting()
+								)
+						);
+
+		System.out.println(studentGroupByAndCount);
+
+
+		//Group by gender – Collectors.groupingBy and Collectors.mapping example.
+		Map<String, List<Student>> groupByGender  = studentList
+				.stream()
+				.collect(
+						Collectors.groupingBy(Student::getGender)
+						);
+
+
+		groupByGender
+		.entrySet()
+		.stream()
+		.forEach(e -> System.out.println(e +"\n\n"));
+		
+		//convert stream to array
+		String lines = "java8 stream lambda functional interface";
+		String [] array = Arrays.stream(lines.split("\\s+"))
+		.map(e -> e).toArray(String[]::new);
+		
+		for(String value : array) {
+			System.out.println(value);
+		}
+		
+		
+		// sorted  by name
+		studentList.stream()
+		.filter(e -> e.getCgpa() > 3.5 && e.getAge() <30)
+		.sorted(Comparator.comparing(Student::getName))
+		.forEach(e -> System.out.println(e));
+//		.collect(Collectors.toList());
+		
+		// reverse sorted by name
+		System.out.println("=============== reverse sorted by name==============");
+		studentList.stream()
+		.filter(e -> e.getCgpa() > 3.5 && e.getAge() <30)
+		.sorted(Comparator.comparing(Student::getName).reversed())
+		.forEach(e -> System.out.println(e));
+		
+		
+		// sum num of array [there 2 ways]
+
 	}
 
 }
